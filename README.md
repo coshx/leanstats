6122f27409011b51437fc90d12a0980658b12547
Ben Taitelbaum <ben@coshx.com>
2012-12-23 23:37:13 -0500

    Initial Import
    rails new leanstats

---
+++ README.md
@@ -0,0 +1,14 @@
+Lean Stats
+==========
+
+  Big Hairy Audacious Goals
+  -------------------------
+
+    * Provide simple tools to access and understand the current status of
+    all the system hypotheses
+
+  Plan
+  ----
+
+    1. Start with most important value hypothesis of this system itself
+    1. Initially, persevere
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leanstats::Application.config.secret_token = '4b292f136960c0c7e2ebdeea8b2a47d538eabe0e0a9887f7a48cbac225cd915ecbd5912cf3044d5703616257f74eee6c0d98c03f280d7191d2ea2252af99b5a1'
